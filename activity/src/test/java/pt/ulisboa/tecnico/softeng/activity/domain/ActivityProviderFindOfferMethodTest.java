6ac416a97d35ec82e0908f66e75f4af25cb510a6
MiguelRenda <miguel.renda@tecnico.ulisboa.pt>
2017-03-15 14:44:38 +0000

    Added tests to ActivityProviderFindOfferMethodTest

--- activity/src/test/java/pt/ulisboa/tecnico/softeng/activity/domain/ActivityProviderFindOfferMethodTest.java
+++ activity/src/test/java/pt/ulisboa/tecnico/softeng/activity/domain/ActivityProviderFindOfferMethodTest.java
@@ -8,6 +8,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import pt.ulisboa.tecnico.softeng.activity.domain.exception.ActivityException;
+
 public class ActivityProviderFindOfferMethodTest {
 	private ActivityProvider provider;
 	private ActivityOffer offer;
@@ -32,7 +34,24 @@ public class ActivityProviderFindOfferMethodTest {
 		Assert.assertEquals(1, offers.size());
 		Assert.assertTrue(offers.contains(this.offer));
 	}
-
+	
+	@Test(expected = ActivityException.class)
+	public void nullActivityTest() {
+		new ActivityOffer(null, new LocalDate(2016, 12, 19),new LocalDate(2016, 12, 21));
+		Assert.fail();
+	}
+	
+	@Test(expected = ActivityException.class)
+	public void nullBeginTest() {
+		this.offer.matchDate(null, new LocalDate(2016, 12, 21));
+		Assert.fail();
+	}
+	
+	@Test(expected = ActivityException.class)
+	public void nullEndTest() {
+		this.offer.matchDate(new LocalDate(2016, 12, 19), null);
+		Assert.fail();
+	}
 	@After
 	public void tearDown() {
 		ActivityProvider.providers.clear();
