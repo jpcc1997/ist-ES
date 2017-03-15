676456e6fce3fa5bfe431a55d81c7126ab49ed3b
ist181647 <mrgalamba@gmail.com>
2017-03-15 16:15:59 +0000

    added tests to HotelHasVacancyMethod

--- hotel/src/test/java/pt/ulisboa/tecnico/softeng/hotel/domain/HotelHasVacancyMethodTest.java
+++ hotel/src/test/java/pt/ulisboa/tecnico/softeng/hotel/domain/HotelHasVacancyMethodTest.java
@@ -26,6 +26,55 @@ public class HotelHasVacancyMethodTest {
 
 		Assert.assertEquals("01", room.getNumber());
 	}
+	
+	@Test
+	public void noVacancy() {
+		LocalDate arrival = new LocalDate(2017, 1, 1);
+		LocalDate departure = new LocalDate(2017, 1, 15);
+		Hotel.reserveHotel(Type.DOUBLE, arrival, departure);
+		Assert.assertNull(hotel.hasVacancy(Type.DOUBLE, arrival, departure));
+	}
+	
+	@Test
+	public void noVacancy2() {
+		LocalDate arrival = new LocalDate(2018, 1, 1);
+		LocalDate departure = new LocalDate(2018, 1, 15);
+		LocalDate arrival2 = new LocalDate(2018, 1, 5);
+		LocalDate departure2 = new LocalDate(2018, 1, 10);
+		Hotel.reserveHotel(Type.DOUBLE, arrival, departure);
+		Assert.assertNull(hotel.hasVacancy(Type.DOUBLE, arrival2, departure2));
+	}
+	
+	@Test
+	public void noVacancy3() {
+		LocalDate arrival = new LocalDate(2019, 1, 5);
+		LocalDate departure = new LocalDate(2019, 1, 25);
+		LocalDate arrival2 = new LocalDate(2019, 1, 1);
+		LocalDate departure2 = new LocalDate(2019, 1, 28);
+		Hotel.reserveHotel(Type.DOUBLE, arrival, departure);
+		Assert.assertNull(hotel.hasVacancy(Type.DOUBLE, arrival2, departure2));
+	}
+	
+	@Test
+	public void noVacancy4() {
+		LocalDate arrival = new LocalDate(2020, 1, 5);
+		LocalDate departure = new LocalDate(2020, 1, 25);
+		LocalDate arrival2 = new LocalDate(2020, 1, 8);
+		LocalDate departure2 = new LocalDate(2020, 1, 28);
+		Hotel.reserveHotel(Type.DOUBLE, arrival, departure);
+		Assert.assertNull(hotel.hasVacancy(Type.DOUBLE, arrival2, departure2));
+	}
+	
+	@Test
+	public void noVacancy5() {
+		new Room(this.hotel, "15", Type.SINGLE);
+		LocalDate arrival = new LocalDate(2021, 1, 5);
+		LocalDate departure = new LocalDate(2021, 1, 25);
+		LocalDate arrival2 = new LocalDate(2021, 1, 1);
+		LocalDate departure2 = new LocalDate(2021, 1, 22);
+		Hotel.reserveHotel(Type.SINGLE, arrival, departure);
+		Assert.assertNull(hotel.hasVacancy(Type.SINGLE, arrival2, departure2));
+	}
 
 	@After
 	public void tearDown() {
