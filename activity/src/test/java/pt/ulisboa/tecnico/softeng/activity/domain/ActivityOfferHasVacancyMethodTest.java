30848f4c6fc11c71536cd106884839fefbf082b6
MiguelRenda <miguel.renda@tecnico.ulisboa.pt>
2017-03-15 14:48:40 +0000

    Added tests to ActivityOfferHasVacancyMethodTest

--- activity/src/test/java/pt/ulisboa/tecnico/softeng/activity/domain/ActivityOfferHasVacancyMethodTest.java
+++ activity/src/test/java/pt/ulisboa/tecnico/softeng/activity/domain/ActivityOfferHasVacancyMethodTest.java
@@ -6,6 +6,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import pt.ulisboa.tecnico.softeng.activity.domain.exception.ActivityException;
+
 public class ActivityOfferHasVacancyMethodTest {
 	private ActivityOffer offer;
 
@@ -24,7 +26,23 @@ public class ActivityOfferHasVacancyMethodTest {
 	public void successZeroBookinks() {
 		Assert.assertTrue(this.offer.hasVacancy());
 	}
-
+	@Test(expected = ActivityException.class)
+	public void nullActivityTest() {
+		new ActivityOffer(null, new LocalDate(2016, 12, 19),new LocalDate(2016, 12, 21));
+		Assert.fail();
+	}
+	
+	@Test(expected = ActivityException.class)
+	public void nullBeginTest() {
+		this.offer.matchDate(null, new LocalDate(2016, 12, 21));
+		Assert.fail();
+	}
+	
+	@Test(expected = ActivityException.class)
+	public void nullEndTest() {
+		this.offer.matchDate(new LocalDate(2016, 12, 19), null);
+		Assert.fail();
+	}
 	@After
 	public void tearDown() {
 		ActivityProvider.providers.clear();
