ead8164ca4a950d3ca26a34115a7f0eb5b4f9d8b
nowitworks <joao.pinto.silveira@gmail.com>
2017-03-24 19:50:55 +0000

    ConfirmedState class partially implemented - missing the information prints

--- broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/ConfirmedState.java
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/ConfirmedState.java
@@ -1,18 +1,85 @@
 package pt.ulisboa.tecnico.softeng.broker.domain;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import pt.ulisboa.tecnico.softeng.activity.dataobjects.ActivityReservationData;
+import pt.ulisboa.tecnico.softeng.activity.exception.ActivityException;
+import pt.ulisboa.tecnico.softeng.bank.dataobjects.BankOperationData;
+import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
 import pt.ulisboa.tecnico.softeng.broker.domain.Adventure.State;
+import pt.ulisboa.tecnico.softeng.broker.exception.RemoteAccessException;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.ActivityInterface;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.BankInterface;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.HotelInterface;
+import pt.ulisboa.tecnico.softeng.hotel.dataobjects.RoomBookingData;
+import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException;
 
 public class ConfirmedState extends AdventureState {
-	
+	private static Logger logger = LoggerFactory.getLogger(CancelledState.class);
+
 	@Override
 	public State getState() {
-		// TODO
-		return null;
+		return State.CONFIRMED;
 	}
 
 	@Override
 	public void process(Adventure adventure) {
-		// TODO
+		logger.debug("process");
+
+		BankOperationData operation;
+		try {
+			operation = BankInterface.getOperationData(adventure.getPaymentConfirmation());
+		} catch (BankException be) {
+			incNumOfRemoteErrors();
+			if (getNumOfRemoteErrors() == 5) {
+				adventure.setState(State.UNDO);
+			}
+			return;
+		} catch (RemoteAccessException rae) {
+			incNumOfRemoteErrors();
+			if (getNumOfRemoteErrors() == 20) {
+				adventure.setState(State.UNDO);
+			}
+			return;
+		}
+		resetNumOfRemoteErrors();
+
+		ActivityReservationData reservation;
+		try {
+			reservation = ActivityInterface.getActivityReservationData(adventure.getActivityConfirmation());
+		} catch (ActivityException ae) {
+			adventure.setState(State.UNDO);
+			return;
+		} catch (RemoteAccessException rae) {
+			incNumOfRemoteErrors();
+			if (getNumOfRemoteErrors() == 20) {
+				adventure.setState(State.UNDO);
+			}
+			return;
+		}
+		resetNumOfRemoteErrors();
+
+		if (adventure.getRoomConfirmation() != null) {
+			RoomBookingData booking;
+			try {
+				booking = HotelInterface.getRoomBookingData(adventure.getRoomConfirmation());
+			} catch (HotelException he) {
+				adventure.setState(State.UNDO);
+				return;
+			} catch (RemoteAccessException rae) {
+				incNumOfRemoteErrors();
+				if (getNumOfRemoteErrors() == 20) {
+					adventure.setState(State.UNDO);
+				}
+				return;
+			}
+			resetNumOfRemoteErrors();
+		}
+
+		// TODO: prints the complete Adventure file, the info in operation,
+		// reservation and booking
+
 	}
 
 }
