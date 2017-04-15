063df501a7638c1bcfc22950382f164393217453
ist181749 <vasco.leal@tecnico.ulisboa.pt>
2017-04-15 19:01:30 +0100

    #103 made Account class atributes persistent

--- bank/src/main/java/pt/ulisboa/tecnico/softeng/bank/domain/Account.java
+++ bank/src/main/java/pt/ulisboa/tecnico/softeng/bank/domain/Account.java
@@ -2,23 +2,23 @@ package pt.ulisboa.tecnico.softeng.bank.domain;
 
 import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
 
-public class Account {
+public class Account extends Account_Base{
 	private static int counter = 0;
 
 	private final Bank bank;
-	private final String IBAN;
+	//private final String IBAN;
 	private final Client client;
-	private int balance;
+	//private int balance;
 
 	public Account(Bank bank, Client client) {
 		checkArguments(bank, client);
 
 		this.bank = bank;
-		this.IBAN = bank.getCode() + Integer.toString(++Account.counter);
+		setIBAN(bank.getCode() + Integer.toString(++Account.counter));
 		this.client = client;
-		this.balance = 0;
-
-		bank.addAccount(this);
+		setBalance(0);
+ 
+		bank.addAccount(this); //fix
 	}
 
 	private void checkArguments(Bank bank, Client client) {
@@ -36,35 +36,35 @@ public class Account {
 		return this.bank;
 	}
 
-	public String getIBAN() {
-		return this.IBAN;
-	}
+//	public String getIBAN() {
+//		return this.IBAN;
+//	}
 
 	public Client getClient() {
 		return this.client;
 	}
 
-	public int getBalance() {
-		return this.balance;
-	}
+//	public int getBalance() {
+//		return this.balance;
+//	}
 
 	public String deposit(int amount) {
 		if (amount <= 0) {
 			throw new BankException();
 		}
-
-		this.balance = this.balance + amount;
+		int balance = getBalance();
+		balance = balance + amount;
 
 		Operation operation = new Operation(Operation.Type.DEPOSIT, this, amount);
 		return operation.getReference();
 	}
 
 	public String withdraw(int amount) {
-		if (amount <= 0 || amount > this.balance) {
+		if (amount <= 0 || amount > getBalance()) {
 			throw new BankException();
 		}
-
-		this.balance = this.balance - amount;
+		int balance = getBalance();
+		balance = balance - amount;
 
 		return new Operation(Operation.Type.WITHDRAW, this, amount).getReference();
 	}
